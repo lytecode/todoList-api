2723fa5b46c01895468c42bb2a4fc5595bd8db43
Basil <mbonubasil@gmail.com>
2019-08-25 18:07:49 +0100

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./config");
+const todoRoutes = require("./routes/todos");
+
+const app = express();
+const PORT = config.PORT;
+
+//connect to database
+mongoose
+  .connect(config.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => console.log("DB connected ...."))
+  .catch(err => console.log("Error connecting to database"));
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//api
+app.use("/api/todos", todoRoutes);
+
+app.listen(PORT, () => console.log(`App started on port ${PORT}`));
