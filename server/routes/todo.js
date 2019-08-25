d448da932a72cc6958f059c72e8c28c684c634b1
Basil <mbonubasil@gmail.com>
2019-08-25 19:33:36 +0100

    Import todo contollers to todo routes

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,4 +1,11 @@
 const todoRouter = require("express").Router();
+const {
+  createTodo,
+  findTodos,
+  findTodo,
+  updateTodo,
+  deleteTodo
+} = require("../controllers/todo");
 
 todoRouter.post("/", createTodo);
 todoRouter.get("/", findTodos);
