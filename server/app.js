ab3d131e8ad560b9d5097aca72e7e020d9f7ea4b
Basil <mbonubasil@gmail.com>
2019-08-26 06:07:02 +0100

    Add auth route to app

--- server/app.js
+++ server/app.js
@@ -3,13 +3,18 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const config = require("./config");
 const todoRoutes = require("./routes/todo");
+const auth = require("./routes/user");
 
 const app = express();
 const PORT = config.PORT;
 
 //connect to database
 mongoose
-  .connect(config.MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .connect(config.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
   .then(() => console.log("DB connected ...."))
   .catch(err => console.log("Error connecting to database"));
 
@@ -18,6 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 //api
+app.use("/api/", auth);
 app.use("/api/todos", todoRoutes);
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
