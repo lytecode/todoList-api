9e9978ceea841db928c26f2b1d4c5b845205e8ee
Basil <mbonubasil@gmail.com>
2019-08-26 06:10:12 +0100

    Read JWT secret and expiry time

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const config = {
   MONGO_URI: process.env.MONGO_URI,
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
+  TOKEN_EXPIRESIN: process.env.TOKEN_EXPIRESIN
 };
 
 module.exports = config;
